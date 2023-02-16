4ae49ee3e66a46d5efc0cd4a5dae0d0256abf927
raphaelin <raphealshina@gmail.com>
2023-02-16 12:30:47 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print function
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
